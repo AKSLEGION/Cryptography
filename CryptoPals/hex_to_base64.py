6d8f6efe82b343543f42d14744f35a7bce8cccbc
AKSLEGION <atish_ks@cs.iitr.ac.in>
2023-08-20 15:07:27 +0530

    a

--- CryptoPals/hex_to_base64.py
+++
@@ -1,8 +0,0 @@
-from base64 import *
-
-def hex_to_base64(hex_str):
-	return b64encode(bytes.fromhex(hex_str)).decode()
-
-#hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-#print(hex_to_base64(hex_str))
-
